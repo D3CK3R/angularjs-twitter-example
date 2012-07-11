58dae029e2c4c4ec156d60c069e281259519f07f
= <jan.zikan@gmail.com>
2012-07-11 18:05:27 +0200

    Deep linking.

--- js/twitter.js
+++ js/twitter.js
@@ -1,6 +1,12 @@
 angular.module('Twitter', ['ngResource']);
 
-function TwitterController($scope, $resource) {
+function TwitterController($scope, $resource, $location) {
+  $scope.$watch('$location.hash()', function() {
+    $scope.username = $location.hash();
+    $scope.fetch();
+  });
+  
+
   $scope.twitter = $resource(
     'https://api.twitter.com/1/users/:action',
     {action:'show.json', callback:'JSON_CALLBACK'},
@@ -12,6 +18,9 @@ function TwitterController($scope, $resource) {
     {
       $scope.userData = $scope.twitter.get({screen_name:$scope.username});
       $scope.screenName = $scope.username;
+
+      // Change the hash in the URL.
+      $location.hash($scope.username);
     }
   };
 }
